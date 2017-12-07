4aa18b2c56ac45a12820b38f01a274adbe79c85b
IsNology <glenn@isnology.com>
2017-12-07 11:04:15 +1100

    add signin form functions

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignInForm from './components/SignInForm'
 
 class App extends Component {
+  onSignIn = ({ email, password }) => {
+    console.log('App received:', { email, password })
+  }
+
+
   render() {
     return (
       <div className="App">
-
+        <h1>Yarra</h1>
+        <h2 className="mb-3">Now Delivering: Shipping Trillions of Products</h2>
+        <SignInForm onSignIn={ this.onSignIn } />
       </div>
     );
   }
---
+++ web/src/components/SignInForm.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+function SignInForm({ onSignIn }) {
+  return (
+      <form
+          onSubmit={ (event) => {
+            event.preventDefault()
+            const form = event.target
+            const elements = form.elements
+            const email = elements.email.value
+            const password = elements.password.value
+            onSignIn({ email, password })
+          }}
+      >
+        <label>
+          {'Email: '}
+          <input
+            type='email'
+            name='email'
+          />
+        </label>
+        <label>
+          { 'password' }
+          <input type='password' id='password' name='password' />
+
+        </label>
+        <button>
+          Sign In
+        </button>
+      </form>
+  )
+}
+
+export default SignInForm
